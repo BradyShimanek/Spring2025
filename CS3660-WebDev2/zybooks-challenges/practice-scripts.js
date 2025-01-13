912557791e23e39691cdccb4cdf58f1697d7a1ca
BradyShimanek <bradyshimanek33@gmail.com>
2025-01-13 14:50:09 -0700

    Added practice files for zybooks

---
+++ CS3660-WebDev2/zybooks-challenges/practice-scripts.js
@@ -0,0 +1,95 @@
+// Arrays
+// let scores = [];
+
+// scores[0] = 6;
+// scores[1] = 15;
+// scores[2] = 8;
+
+// console.log(scores[0]);
+// console.log(scores[1]);
+// console.log(scores[2]);
+
+// let teams = ["Tigers", "Bisons", 
+//              "Eagles", "Cobras"];
+
+// console.log(teams);
+
+
+
+
+// People waiting in line (Kylo is in front, Leia at the end)
+// let line = ["Kylo", "Finn", "Rey", "Maz", "Leia"];
+
+// line.shift();
+// x = line.pop();
+// line.unshift(x);
+
+// line.splice(2, 0, "Poe", "Snoke");
+// line.splice(4, 1);
+// line.push("Han");
+
+// // Show entire line
+// console.log(line);
+
+
+
+// FOR LOOPS
+
+// let groceries = ["bread", "milk", "peanut butter"];
+
+// // Display all elements in groceries array
+// for (i = 0; i < groceries.length; i++) {
+//    console.log(i + " - " + groceries[i]);
+// }
+
+
+// let groceries = ["bread", "milk", "peanut butter"];
+
+// // Display all elements in groceries array
+// for (let item of groceries) {
+//    console.log(item);
+// }
+
+//  let groceries = ["bread", "milk", "peanut butter"];
+
+// // Display all elements in groceries array
+// groceries.forEach(function(item, index) {
+//    console.log(index + " - " + item);
+// });
+
+
+// let dukeScores  = [72, 74, 84, 92, 93, 66, 69, 73, 70, 85, 75, 67, 79];
+// let ncScores    = [76, 73, 77, 90, 81, 74, 53, 68, 88, 84, 58, 81, 73];
+// let winningTeam = [];
+
+// for (i = 0; i < dukeScores.length; i++) {
+//     if (dukeScores[i] > ncScores[i]) {
+//         winningTeam.push("D");
+//     }
+//     else {
+//         winningTeam.push("N");
+//     }
+// }
+
+// for (let result of winningTeam) {
+//     console.log(result)
+// }
+
+// // Who won the first game?
+// if (dukeScores[0] > ncScores[0]) {
+//    console.log("Duke won " + dukeScores[0] + "-" + ncScores[0] + ".");
+// }
+// else {
+//    console.log("North Carolina won " + ncScores[0] + "-" + dukeScores[0] + ".");
+// }
+
+
+
+// SORTING
+// let totals = [90, 4, 21, 34];
+// totals.sort();
+// console.log(totals);
+//output is [ 21, 34, 4, 90 ] because the first number of each number is what is compared
+
+
+
